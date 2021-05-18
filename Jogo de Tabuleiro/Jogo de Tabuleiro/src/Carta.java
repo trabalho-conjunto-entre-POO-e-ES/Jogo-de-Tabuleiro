ccc617a3517defe429973e37dc852db0b588f1c3
JPSFelicio <jopesafe24@gmail.com>
2021-05-18 14:45:17 +0100

    Atualização do documento classes
    adicionado o detalhe extends no ficheiro classes para melhor orientação

--- Jogo de Tabuleiro/Jogo de Tabuleiro/src/Carta.java
+++ Jogo de Tabuleiro/Jogo de Tabuleiro/src/Carta.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
 
 public class Carta {
+	// ATRIBUTOS
+ArrayList<String> poder = new ArrayList<String>();
+ int nivel[] = new int[6];
+	//ACESSORES
 
+	//CONSTRUTOR - DEFAULT
+
+	//CONTRUTOR - COM PARAMETROS
+
+	//CONSTRUTOR - COPIA
+
+	//COMPORTAMENTOS
+
+	//COMPLEMENTARES
 }
--- Jogo de Tabuleiro/Jogo de Tabuleiro/src/CartaEspecial.java
+++ Jogo de Tabuleiro/Jogo de Tabuleiro/src/CartaEspecial.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
 
-public class CartaEspecial {
+public class CartaEspecial extends Carta {
+	//ATRIBUTOS
+	ArrayList<String> capacidade = new ArrayList<String>();
+	
+	//ACESSORES
 
+	//CONSTRUTOR - DEFAULT
+
+	//CONTRUTOR - COM PARAMETROS
+
+	//CONSTRUTOR - COPIA
+
+	//COMPORTAMENTOS
+
+	//COMPLEMENTARES
 }
