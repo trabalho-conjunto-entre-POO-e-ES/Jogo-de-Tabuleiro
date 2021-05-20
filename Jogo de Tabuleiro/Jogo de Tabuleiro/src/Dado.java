1cca39f20d9356631c845482c3c3bcecf42665db
Gabriel Lopes <200100291@esg.ipsantarem.pt>
2021-05-20 15:45:20 +0100

    Update Classes
    Atualização da classe Dado do projeto.

--- Jogo de Tabuleiro/Jogo de Tabuleiro/src/Dado.java
+++ Jogo de Tabuleiro/Jogo de Tabuleiro/src/Dado.java
@@ -1,16 +1,52 @@
+import java.util.Random;
 
 public class Dado {
-	//ATRIBUTOS
+	// ATRIBUTOS
+	private int vezesLancado; // vezes que o dado foi lançado
+	private int lados; // lados = sides
+	private Random random;
 
-	//ACESSORES
+	// CONSTRUTOR - DEFAULT
+	public Dado() {
 
-	//CONSTRUTOR - DEFAULT
+	}
 
-	//CONTRUTOR - COM PARAMETROS
+	// CONTRUTOR - COM PARAMETROS
+	public Dado(int sides) {
+		vezesLancado = 0;
+		lados = sides;
+		random = new Random();
+	}
 
-	//CONSTRUTOR - COPIA
+	// ACESSORES
+	public int getVezesLancado() {
+		return vezesLancado;
+	}
 
-	//COMPORTAMENTOS
+	public int getLados() {
+		return lados;
+	}
 
-	//COMPLEMENTARES
+	// COMPORTAMENTOS
+	public int lancar() {
+		vezesLancado++;
+		return random.nextInt(lados) + 1;
+	}
+
+	public void lancamento() {
+		Dado dado = new Dado(6);
+		
+		for(int i = 0; i < 1; i++)
+		{
+		    int rolar = dado.lancar();
+		    System.out.println("Lançar" + dado.getVezesLancado() + ": " + rolar);
+		}
+		
+	}
+
+	// COMPLEMENTARES
+	public String toString() {
+		return "# lados: " + getLados() + " # lancamentos: " + getVezesLancado();
+	}
+
 }
