eb9446a302c701f897e25f50ba40efca40e864b7
JPSFelicio <jopesafe24@gmail.com>
2021-05-20 15:03:40 +0100

    classe jogador
    complementação da classe jogador

--- Jogo de Tabuleiro/Jogo de Tabuleiro/src/Jogador.java
+++ Jogo de Tabuleiro/Jogo de Tabuleiro/src/Jogador.java
@@ -1,16 +1,61 @@
+import java.util.Arrays;
+
+import javax.swing.JOptionPane;
 
 public class Jogador {
 	//ATRIBUTOS
+	private String jogador[] = new String[2];
 
 	//ACESSORES
+	public String[] getJogador() {
+		return jogador;
+	}
+
+	public void setJogador(String[] jogador) {
+		this.jogador = jogador;
+	}
 
 	//CONSTRUTOR - DEFAULT
-
+	public Jogador() {
+		
+	}
 	//CONTRUTOR - COM PARAMETROS
-
+	public Jogador(String[] jogador) {
+		super();
+		this.jogador = jogador;
+	}
 	//CONSTRUTOR - COPIA
+	
 
 	//COMPORTAMENTOS
+	public void nomeJogador() {
+	for ( int i=0 ; i<= jogador.length ; i++ ) {
+		JOptionPane.showInputDialog("nome do jogador" + jogador);
+	    }
+	}
+
 
 	//COMPLEMENTARES
+	@Override
+	public String toString() {
+		return "Jogador [jogador=" + Arrays.toString(jogador) + "]";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Jogador other = (Jogador) obj;
+		if (!Arrays.equals(jogador, other.jogador))
+			return false;
+		return true;
+	}
+
+
+	
 }
+
