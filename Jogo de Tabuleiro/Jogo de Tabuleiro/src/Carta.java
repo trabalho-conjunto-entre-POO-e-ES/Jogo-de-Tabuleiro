8e7551944858ca2ac0d1f73713c20da14347dd4b
Gabriel Lopes <79711945+borreicho95@users.noreply.github.com>
2021-05-21 16:14:45 +0100

    Update Carta.java
    Atualização da classe Carta (não acabada)

--- Jogo de Tabuleiro/Jogo de Tabuleiro/src/Carta.java
+++ Jogo de Tabuleiro/Jogo de Tabuleiro/src/Carta.java
@@ -1,18 +1,48 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class Carta extends Cartas{
+public class Carta {
 	// ATRIBUTOS
-ArrayList<String> poder = new ArrayList<String>();
- int nivel[] = new int[6];
-	//ACESSORES
+	ArrayList<String> poder = new ArrayList<String>();
+	int nivel[] = new int[6];
+	
+	// ACESSORES
+	public ArrayList<String> getPoder() {
+		return poder;
+	}
+	public void setPoder(ArrayList<String> poder) {
+		this.poder = poder;
+	}
+	public int[] getNivel() {
+		return nivel;
+	}
+	public void setNivel(int[] nivel) {
+		this.nivel = nivel;
+	}
 
-	//CONSTRUTOR - DEFAULT
+	// CONSTRUTOR - DEFAULT
+	public Carta() {
+		
+	}
+	
+	// CONTRUTOR - COM PARAMETROS
+	public Carta(ArrayList<String> poder, int[] nivel) {
+		super();
+		this.poder = poder;
+		this.nivel = nivel;
+	}
+	
+	
+	// CONSTRUTOR - COPIA
 
-	//CONTRUTOR - COM PARAMETROS
-
-	//CONSTRUTOR - COPIA
-
-	//COMPORTAMENTOS
-
-	//COMPLEMENTARES
+	// COMPORTAMENTOS
+	public int comparar() {
+		
+	}
+	
+	// COMPLEMENTARES
+	@Override
+	public String toString() {
+		return "Carta [poder=" + poder + ", nivel=" + Arrays.toString(nivel) + "]";
+	}
 }
