3f24d96b132cc5c6e900ea1009404b81b874084b
JPSFelicio <jopesafe24@gmail.com>
2021-06-04 16:27:53 +0100

    identificação das classes e inicio da classe casas e tabuleiro
    identificação no documento classes as que se encontram completas e incompletas.
    inicio da construção das classes casas e tabuleiro.

--- Jogo de Tabuleiro/Jogo de Tabuleiro/src/Tabuleiro.java
+++ Jogo de Tabuleiro/Jogo de Tabuleiro/src/Tabuleiro.java
@@ -3,12 +3,28 @@ import java.util.ArrayList;
 public class Tabuleiro {
 	//ATRIBUTOS
 	ArrayList<Casas> casas = new ArrayList<Casas>();
+
 	
 	//ACESSORES
+	public ArrayList<Casas> getCasas() {
+		return casas;
+	}
+
+	public void setCasas(ArrayList<Casas> casas) {
+		this.casas = casas;
+	}
 
 	//CONSTRUTOR - DEFAULT
+	public Tabuleiro() {
+		
+	}
+
 
 	//CONTRUTOR - COM PARAMETROS
+	public Tabuleiro(ArrayList<Casas> casas) {
+		super();
+		this.casas = casas;
+	}
 
 	//CONSTRUTOR - COPIA
 
